9d8d92c66cd0b9d6965adb8ea6b446aff5a1ecfa
saranyaamani <saranyakavya1631@gmail.com>
2017-06-08 18:22:00 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class large
+{
+public static void main(string args[])
+{
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+if((a>=b)&&(a>=c))
+{
+System.out.println(a);
+}
+else if(b>=c)&&(b>=a)
+{
+System.out.println(b);
+}
+else
+{
+System.out.println(c);
+}
+}
+}
