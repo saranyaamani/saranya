2b904b6e96dd81d9628b124bc641a4b655608e59
saranyaamani <saranyakavya1631@gmail.com>
2017-06-08 18:44:14 +0530

    Create num.java

---
+++ num.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class num
+{
+public static void main(string args[])
+{
+Scanner scan=new Scan(System.in);
+int a=scan.nextInt();
+for(i=0;i<n;i++)
+{
+if(a[i]>=0)
+{
+System.out.println(sum);
+}
+else
+{
+System.out.println();
+}
+}
+}
