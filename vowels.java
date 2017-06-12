df0c920f40a3fa4554215b3f0a6437f07e90c0f9
saranyaamani <saranyakavya1631@gmail.com>
2017-06-12 12:08:52 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class vowels
+{
+public static void main(String args[])
+{
+char n;
+Scanner scan=new Scanner(System.in)
+System.out.println("enter the element");
+
