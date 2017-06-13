0b97f1d94c8e2253e1c391ac4c62ebfb4e96bf62
saranyaamani <saranyakavya1631@gmail.com>
2017-06-13 11:34:20 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+public class digit
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+int n=0;
+int a;
+n=scan.nextInt();
+if(n<0)
+{
+n=n*-1';
+}
+else(n==0)
+{
+  n=1;
+  }
+  while(n>0)
+  {
+  n=n/10;
+  a++
+  }
+  System.out.println("the number of digits in the given num");
+  }
+  }
