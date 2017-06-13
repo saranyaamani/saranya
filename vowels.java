54cc807e2c90724bd83f4723bbe615d55d18ab0b
saranyaamani <saranyakavya1631@gmail.com>
2017-06-13 10:19:52 +0530

    Create vowels.java

--- vowels.java
+++ vowels.java
@@ -1,40 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-public class Vowel_Consonant 
+import java.io.*;
+import java.util.*
+public class number
 {
-    public static void main(String[] args) throws Exception 
+    public static void main(String args[])
     {
         char n;
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Enter the character you want to check:");
-        n = (char) bf.read();
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the elements");
+        n=(char)scan.read()
         switch(n)
-        {
-            case 'a':
-            System.out.println("The given character "+n+" is vowel");
-            break;
- 
-            case 'e':
-            System.out.println("The given character "+n+" is vowel");
-            break;
- 
-            case 'i':
-            System.out.println("The given character "+n+" is vowel");
-            break;
- 
-            case 'o':
-            System.out.println("The given character "+n+" is vowel");
-            break;
- 
-            case 'u':
-            System.out.println("The given character "+n+" is vowel");
-            break;
- 
-            default:
-            System.out.println("The given character "+n+" is consonant");
-            break;
-        }
+            case a:
+                System.out.println("the given character is vowel");
+                break;
+            case e:
+               System.out.println("the given character is vowel");
+               break;
+            case i:
+               System.out.println("the given character is vowel");
+               braek;
+            case o:
+               System.out.printl("the given character is vowel");
+               break;
+        case u:
+               System.out.println("the given character is vowel");
+               break;
+        default
+               System.out.println("the given character is consonant ");
+               break;
     }
+}
 }
